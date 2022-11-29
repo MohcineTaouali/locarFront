fb73ecf0aaa001e59327d71345e6c0aa6ad12efb
taouali <mouhcine.taouali@gmail.com>
2022-11-29 22:38:18 +0100

    adaptation angular with some change in version of nodejs

--- src/app/features/listing-cars/listing-cars.component.ts
+++ src/app/features/listing-cars/listing-cars.component.ts
@@ -10,6 +10,15 @@ export class ListingCarsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(function() {
+      $('.flip-btn').on( 'click', function() {
+        $( this ).parent().toggleClass('flip-active');
+      });
+    
+      $('.flip-btn-hide').on( 'click', function() {
+        $(this).parents('.b-goods').removeClass('flip-active');
+      });
+    });
   }
 
 }
